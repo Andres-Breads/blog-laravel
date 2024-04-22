f11cd66b14bf04a36e004942657193acc31d9002
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-04-22 17:32:12 -0500

    redirecting dashboard to posts when logged

--- routes/web.php
+++ routes/web.php
@@ -14,9 +14,7 @@ Route::controller(PageController::class)->group(function () {
     Route::get('blog/{post:slug}', 'post')->name('post');
 });
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::redirect('dashboard', 'posts')->name('dashboard');
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
@@ -24,6 +22,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::resource('posts', PostController::class)->except(['show'])->middleware(['auth']);
+Route::resource('posts', PostController::class)->middleware(['auth', 'verified'])->except(['show']);
 
 require __DIR__.'/auth.php';
