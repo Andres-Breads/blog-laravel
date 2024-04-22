e743cd8637a18771efe2a4c4d7078cdd987e5e3e
Carlos Aucaruri <carlos.aucaruri@gmail.com>
2024-04-21 22:28:48 -0500

    added dashboard panel as index for posts

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\PageController;
+use App\Http\Controllers\PostController;
 use Illuminate\Support\Facades\Route;
 
 /* Route::get('/', function () {
@@ -23,5 +24,7 @@ Route::middleware('auth')->group(function () {
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
+
+Route::resource('posts', PostController::class)->except(['show'])->middleware(['auth']);
 
 require __DIR__.'/auth.php';
